1c326944b0c74be85c9b374466a7f3061c1e91a3
celinayk <celina.16161616@gmail.com>
2024-11-18 22:55:48 +0900

    초기환경

---
+++ src/main/java/com/munhwahansang/back/TestController.java
@@ -0,0 +1,13 @@
+package com.munhwahansang.back;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "안녕하세요 문화한상입니다~";
+    }
+}
