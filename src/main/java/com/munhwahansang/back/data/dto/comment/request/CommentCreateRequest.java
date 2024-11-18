f72b47e219898aab75aafbfe9e29636a4f067b33
masiljangajji <xmfpdlsj0508@gmail.com>
2024-11-19 00:26:13 +0900

    feat: (Comment) DTO 추가

---
+++ src/main/java/com/munhwahansang/back/data/dto/comment/request/CommentCreateRequest.java
@@ -0,0 +1,4 @@
+package com.munhwahansang.back.data.dto.comment.request;
+
+public record CommentCreateRequest(String content) {
+}
---
+++ src/main/java/com/munhwahansang/back/data/dto/comment/response/CommentFindResponse.java
@@ -0,0 +1,4 @@
+package com.munhwahansang.back.data.dto.comment.response;
+
+public record CommentFindResponse(String userName,String content) {
+}
