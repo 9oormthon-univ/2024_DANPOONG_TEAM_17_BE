af223863ca4c13e0d7dc0d86530da818851ea3ce
celinayk <celina.16161616@gmail.com>
2024-11-22 02:59:59 +0900

    feat(config):프론트 주소 수정

--- src/main/java/com/munhwahansang/back/config/WebConfig.java
+++ src/main/java/com/munhwahansang/back/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("https://2024-danpoong-team-17-fe.vercel.app", "http://localhost:3000", "https://www.munhwahansang.com") // Vercel 도메인 허용
+                .allowedOrigins("https://2024-danpoong-team-17-fe.vercel.app", "http://localhost:5173", "https://www.munhwahansang.com") // Vercel 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보(쿠키 등) 허용
