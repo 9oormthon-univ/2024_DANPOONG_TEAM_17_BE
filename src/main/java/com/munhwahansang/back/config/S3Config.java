f3282aa5b496b3ea2ef1cc7297f44a5ce81e2eff
masiljangajji <xmfpdlsj0508@gmail.com>
2024-11-23 23:45:58 +0900

    hotfix: s3 config

--- src/main/java/com/munhwahansang/back/config/S3Config.java
+++ src/main/java/com/munhwahansang/back/config/S3Config.java
@@ -11,10 +11,10 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-Key}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-Key}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
     private final Region region = Region.AP_NORTHEAST_2; // 서울 리전
